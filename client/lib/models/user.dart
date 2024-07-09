699e69d0431a1fd1dc5b6d7b674f3c30dfc52b4b
Brandon Redmond <brandon@gethealthie.om>
2024-07-09 13:11:07 -0300

    Created Home Page on Flutter

---
+++ client/lib/models/user.dart
@@ -0,0 +1,16 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String password;
+  final String skillLevel;
+  final String gym;
+
+  User(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.password,
+      required this.skillLevel,
+      required this.gym});
+}
