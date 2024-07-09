aa79387fa95af2cd7310e34df4de741a11b3aa7d
Brandon Redmond <brandon@gethealthie.om>
2024-07-09 14:40:45 -0300

    Created GraphQL Query Dart Files

---
+++ client/lib/screens/home_screen/fetchUsers.graphql.dart
@@ -0,0 +1,453 @@
+import 'package:gql/ast.dart';
+
+class Query$FetchUsers {
+  Query$FetchUsers({
+    required this.users,
+    this.$__typename = 'Query',
+  });
+
+  factory Query$FetchUsers.fromJson(Map<String, dynamic> json) {
+    final l$users = json['users'];
+    final l$$__typename = json['__typename'];
+    return Query$FetchUsers(
+      users: (l$users as List<dynamic>)
+          .map((e) => e == null
+              ? null
+              : Query$FetchUsers$users.fromJson((e as Map<String, dynamic>)))
+          .toList(),
+      $__typename: (l$$__typename as String),
+    );
+  }
+
+  final List<Query$FetchUsers$users?> users;
+
+  final String $__typename;
+
+  Map<String, dynamic> toJson() {
+    final _resultData = <String, dynamic>{};
+    final l$users = users;
+    _resultData['users'] = l$users.map((e) => e?.toJson()).toList();
+    final l$$__typename = $__typename;
+    _resultData['__typename'] = l$$__typename;
+    return _resultData;
+  }
+
+  @override
+  int get hashCode {
+    final l$users = users;
+    final l$$__typename = $__typename;
+    return Object.hashAll([
+      Object.hashAll(l$users.map((v) => v)),
+      l$$__typename,
+    ]);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (!(other is Query$FetchUsers) || runtimeType != other.runtimeType) {
+      return false;
+    }
+    final l$users = users;
+    final lOther$users = other.users;
+    if (l$users.length != lOther$users.length) {
+      return false;
+    }
+    for (int i = 0; i < l$users.length; i++) {
+      final l$users$entry = l$users[i];
+      final lOther$users$entry = lOther$users[i];
+      if (l$users$entry != lOther$users$entry) {
+        return false;
+      }
+    }
+    final l$$__typename = $__typename;
+    final lOther$$__typename = other.$__typename;
+    if (l$$__typename != lOther$$__typename) {
+      return false;
+    }
+    return true;
+  }
+}
+
+extension UtilityExtension$Query$FetchUsers on Query$FetchUsers {
+  CopyWith$Query$FetchUsers<Query$FetchUsers> get copyWith =>
+      CopyWith$Query$FetchUsers(
+        this,
+        (i) => i,
+      );
+}
+
+abstract class CopyWith$Query$FetchUsers<TRes> {
+  factory CopyWith$Query$FetchUsers(
+    Query$FetchUsers instance,
+    TRes Function(Query$FetchUsers) then,
+  ) = _CopyWithImpl$Query$FetchUsers;
+
+  factory CopyWith$Query$FetchUsers.stub(TRes res) =
+      _CopyWithStubImpl$Query$FetchUsers;
+
+  TRes call({
+    List<Query$FetchUsers$users?>? users,
+    String? $__typename,
+  });
+  TRes users(
+      Iterable<Query$FetchUsers$users?> Function(
+              Iterable<
+                  CopyWith$Query$FetchUsers$users<Query$FetchUsers$users>?>)
+          _fn);
+}
+
+class _CopyWithImpl$Query$FetchUsers<TRes>
+    implements CopyWith$Query$FetchUsers<TRes> {
+  _CopyWithImpl$Query$FetchUsers(
+    this._instance,
+    this._then,
+  );
+
+  final Query$FetchUsers _instance;
+
+  final TRes Function(Query$FetchUsers) _then;
+
+  static const _undefined = <dynamic, dynamic>{};
+
+  TRes call({
+    Object? users = _undefined,
+    Object? $__typename = _undefined,
+  }) =>
+      _then(Query$FetchUsers(
+        users: users == _undefined || users == null
+            ? _instance.users
+            : (users as List<Query$FetchUsers$users?>),
+        $__typename: $__typename == _undefined || $__typename == null
+            ? _instance.$__typename
+            : ($__typename as String),
+      ));
+
+  TRes users(
+          Iterable<Query$FetchUsers$users?> Function(
+                  Iterable<
+                      CopyWith$Query$FetchUsers$users<Query$FetchUsers$users>?>)
+              _fn) =>
+      call(
+          users: _fn(_instance.users.map((e) => e == null
+              ? null
+              : CopyWith$Query$FetchUsers$users(
+                  e,
+                  (i) => i,
+                ))).toList());
+}
+
+class _CopyWithStubImpl$Query$FetchUsers<TRes>
+    implements CopyWith$Query$FetchUsers<TRes> {
+  _CopyWithStubImpl$Query$FetchUsers(this._res);
+
+  TRes _res;
+
+  call({
+    List<Query$FetchUsers$users?>? users,
+    String? $__typename,
+  }) =>
+      _res;
+
+  users(_fn) => _res;
+}
+
+const documentNodeQueryFetchUsers = DocumentNode(definitions: [
+  OperationDefinitionNode(
+    type: OperationType.query,
+    name: NameNode(value: 'FetchUsers'),
+    variableDefinitions: [],
+    directives: [],
+    selectionSet: SelectionSetNode(selections: [
+      FieldNode(
+        name: NameNode(value: 'users'),
+        alias: null,
+        arguments: [],
+        directives: [],
+        selectionSet: SelectionSetNode(selections: [
+          FieldNode(
+            name: NameNode(value: 'id'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: null,
+          ),
+          FieldNode(
+            name: NameNode(value: 'name'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: null,
+          ),
+          FieldNode(
+            name: NameNode(value: 'email'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: null,
+          ),
+          FieldNode(
+            name: NameNode(value: 'skillLevel'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: null,
+          ),
+          FieldNode(
+            name: NameNode(value: 'preferredClimbingStyle'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: null,
+          ),
+          FieldNode(
+            name: NameNode(value: 'preferredGym'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: null,
+          ),
+          FieldNode(
+            name: NameNode(value: '__typename'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: null,
+          ),
+        ]),
+      ),
+      FieldNode(
+        name: NameNode(value: '__typename'),
+        alias: null,
+        arguments: [],
+        directives: [],
+        selectionSet: null,
+      ),
+    ]),
+  ),
+]);
+
+class Query$FetchUsers$users {
+  Query$FetchUsers$users({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.skillLevel,
+    required this.preferredClimbingStyle,
+    required this.preferredGym,
+    this.$__typename = 'User',
+  });
+
+  factory Query$FetchUsers$users.fromJson(Map<String, dynamic> json) {
+    final l$id = json['id'];
+    final l$name = json['name'];
+    final l$email = json['email'];
+    final l$skillLevel = json['skillLevel'];
+    final l$preferredClimbingStyle = json['preferredClimbingStyle'];
+    final l$preferredGym = json['preferredGym'];
+    final l$$__typename = json['__typename'];
+    return Query$FetchUsers$users(
+      id: (l$id as String),
+      name: (l$name as String),
+      email: (l$email as String),
+      skillLevel: (l$skillLevel as String),
+      preferredClimbingStyle: (l$preferredClimbingStyle as String),
+      preferredGym: (l$preferredGym as String),
+      $__typename: (l$$__typename as String),
+    );
+  }
+
+  final String id;
+
+  final String name;
+
+  final String email;
+
+  final String skillLevel;
+
+  final String preferredClimbingStyle;
+
+  final String preferredGym;
+
+  final String $__typename;
+
+  Map<String, dynamic> toJson() {
+    final _resultData = <String, dynamic>{};
+    final l$id = id;
+    _resultData['id'] = l$id;
+    final l$name = name;
+    _resultData['name'] = l$name;
+    final l$email = email;
+    _resultData['email'] = l$email;
+    final l$skillLevel = skillLevel;
+    _resultData['skillLevel'] = l$skillLevel;
+    final l$preferredClimbingStyle = preferredClimbingStyle;
+    _resultData['preferredClimbingStyle'] = l$preferredClimbingStyle;
+    final l$preferredGym = preferredGym;
+    _resultData['preferredGym'] = l$preferredGym;
+    final l$$__typename = $__typename;
+    _resultData['__typename'] = l$$__typename;
+    return _resultData;
+  }
+
+  @override
+  int get hashCode {
+    final l$id = id;
+    final l$name = name;
+    final l$email = email;
+    final l$skillLevel = skillLevel;
+    final l$preferredClimbingStyle = preferredClimbingStyle;
+    final l$preferredGym = preferredGym;
+    final l$$__typename = $__typename;
+    return Object.hashAll([
+      l$id,
+      l$name,
+      l$email,
+      l$skillLevel,
+      l$preferredClimbingStyle,
+      l$preferredGym,
+      l$$__typename,
+    ]);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (!(other is Query$FetchUsers$users) ||
+        runtimeType != other.runtimeType) {
+      return false;
+    }
+    final l$id = id;
+    final lOther$id = other.id;
+    if (l$id != lOther$id) {
+      return false;
+    }
+    final l$name = name;
+    final lOther$name = other.name;
+    if (l$name != lOther$name) {
+      return false;
+    }
+    final l$email = email;
+    final lOther$email = other.email;
+    if (l$email != lOther$email) {
+      return false;
+    }
+    final l$skillLevel = skillLevel;
+    final lOther$skillLevel = other.skillLevel;
+    if (l$skillLevel != lOther$skillLevel) {
+      return false;
+    }
+    final l$preferredClimbingStyle = preferredClimbingStyle;
+    final lOther$preferredClimbingStyle = other.preferredClimbingStyle;
+    if (l$preferredClimbingStyle != lOther$preferredClimbingStyle) {
+      return false;
+    }
+    final l$preferredGym = preferredGym;
+    final lOther$preferredGym = other.preferredGym;
+    if (l$preferredGym != lOther$preferredGym) {
+      return false;
+    }
+    final l$$__typename = $__typename;
+    final lOther$$__typename = other.$__typename;
+    if (l$$__typename != lOther$$__typename) {
+      return false;
+    }
+    return true;
+  }
+}
+
+extension UtilityExtension$Query$FetchUsers$users on Query$FetchUsers$users {
+  CopyWith$Query$FetchUsers$users<Query$FetchUsers$users> get copyWith =>
+      CopyWith$Query$FetchUsers$users(
+        this,
+        (i) => i,
+      );
+}
+
+abstract class CopyWith$Query$FetchUsers$users<TRes> {
+  factory CopyWith$Query$FetchUsers$users(
+    Query$FetchUsers$users instance,
+    TRes Function(Query$FetchUsers$users) then,
+  ) = _CopyWithImpl$Query$FetchUsers$users;
+
+  factory CopyWith$Query$FetchUsers$users.stub(TRes res) =
+      _CopyWithStubImpl$Query$FetchUsers$users;
+
+  TRes call({
+    String? id,
+    String? name,
+    String? email,
+    String? skillLevel,
+    String? preferredClimbingStyle,
+    String? preferredGym,
+    String? $__typename,
+  });
+}
+
+class _CopyWithImpl$Query$FetchUsers$users<TRes>
+    implements CopyWith$Query$FetchUsers$users<TRes> {
+  _CopyWithImpl$Query$FetchUsers$users(
+    this._instance,
+    this._then,
+  );
+
+  final Query$FetchUsers$users _instance;
+
+  final TRes Function(Query$FetchUsers$users) _then;
+
+  static const _undefined = <dynamic, dynamic>{};
+
+  TRes call({
+    Object? id = _undefined,
+    Object? name = _undefined,
+    Object? email = _undefined,
+    Object? skillLevel = _undefined,
+    Object? preferredClimbingStyle = _undefined,
+    Object? preferredGym = _undefined,
+    Object? $__typename = _undefined,
+  }) =>
+      _then(Query$FetchUsers$users(
+        id: id == _undefined || id == null ? _instance.id : (id as String),
+        name: name == _undefined || name == null
+            ? _instance.name
+            : (name as String),
+        email: email == _undefined || email == null
+            ? _instance.email
+            : (email as String),
+        skillLevel: skillLevel == _undefined || skillLevel == null
+            ? _instance.skillLevel
+            : (skillLevel as String),
+        preferredClimbingStyle: preferredClimbingStyle == _undefined ||
+                preferredClimbingStyle == null
+            ? _instance.preferredClimbingStyle
+            : (preferredClimbingStyle as String),
+        preferredGym: preferredGym == _undefined || preferredGym == null
+            ? _instance.preferredGym
+            : (preferredGym as String),
+        $__typename: $__typename == _undefined || $__typename == null
+            ? _instance.$__typename
+            : ($__typename as String),
+      ));
+}
+
+class _CopyWithStubImpl$Query$FetchUsers$users<TRes>
+    implements CopyWith$Query$FetchUsers$users<TRes> {
+  _CopyWithStubImpl$Query$FetchUsers$users(this._res);
+
+  TRes _res;
+
+  call({
+    String? id,
+    String? name,
+    String? email,
+    String? skillLevel,
+    String? preferredClimbingStyle,
+    String? preferredGym,
+    String? $__typename,
+  }) =>
+      _res;
+}
