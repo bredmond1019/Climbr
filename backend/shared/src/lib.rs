6a2eb2609b2d556b245c74f163faea328ebbb4e9
Brandon <bredmond1019@gmail.com>
2024-07-17 13:01:25 -0300

    Shared Lib with User Built

--- backend/shared/src/lib.rs
+++ backend/shared/src/lib.rs
@@ -8,29 +8,27 @@ use std::env;
 
 // use actix::Actor;
 use actix_cors::Cors;
-use actix_web::{middleware::Logger, web, web::Data, App, HttpServer};
+use actix_web::web;
+use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 
-use diesel::r2d2::{ConnectionManager, Pool};
+use db::DbPool;
+use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
 use diesel::PgConnection;
 use dotenv::dotenv;
+use log::info;
+use models::user::User;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+pub async fn get_user(pool: web::Data<DbPool>) -> Vec<User> {
+    let mut conn: PooledConnection<ConnectionManager<PgConnection>> = pool.get().unwrap();
+    let all_users: Result<Vec<User>, diesel::result::Error> =
+        web::block(move || User::find_all(&mut conn)).await.unwrap();
+
+    all_users.unwrap()
+}
+
+pub fn establish_connection() -> PgConnection {
     dotenv().ok();
-    env::set_var("RUST_LOG", "info");
-    env::set_var("RUST_BACKTRACE", "1");
-    env_logger::init();
-
-    let pool: Pool<ConnectionManager<PgConnection>> = db::init_pool();
-    // let schema = Arc::new(create_schema());
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(Data::new(pool.clone()))
-            .wrap(Logger::default())
-            .wrap(Cors::permissive())
-    })
-    .bind("0.0.0.0:3000")?
-    .run()
-    .await
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect("Error connecting to the database")
 }
