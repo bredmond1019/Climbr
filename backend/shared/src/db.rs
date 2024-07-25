1a95a89a9d01a4586743eeeefc61c7211f05759e
Brandon Redmond <brandon@gethealthie.om>
2024-07-25 06:03:56 -0300

    code_cleanup

--- backend/shared/src/db.rs
+++ backend/shared/src/db.rs
@@ -6,8 +6,7 @@ pub type DbPool = Pool<ConnectionManager<PgConnection>>;
 
 pub fn init_pool() -> DbPool {
     let database_url: String = get_database_url();
-    let manager: ConnectionManager<PgConnection> =
-        ConnectionManager::<PgConnection>::new(database_url);
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
     r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create pool.")
